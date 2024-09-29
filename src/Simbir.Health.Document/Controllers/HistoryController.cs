05054ce1904d35bb6fee6d0443a2e165f3f39305
NotScream73 <112487179+NotScream73@users.noreply.github.com>
2024-09-29 19:27:09 +0400

    Init projects to microservices (#1)

---
+++ src/Simbir.Health.Document/Controllers/HistoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simbir.Health.Document.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistoryController : ControllerBase
+    {
+    }
+}
