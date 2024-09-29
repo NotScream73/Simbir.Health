5f4790e6cc5110b7950b3c3590a68c54ad4e8bcd
Никита Сергеев <notscreamplay73@gmail.com>
2024-09-29 19:16:00 +0400

    Init projects to microservices

---
+++ src/Simbir.Health.Hospital/Controllers/HospitalsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simbir.Health.Hospital.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HospitalsController : ControllerBase
+    {
+    }
+}
---
+++ src/Simbir.Health.Timetable/Controllers/TimetablesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simbir.Health.Timetable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimetablesController : ControllerBase
+    {
+    }
+}
